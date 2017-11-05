bb90ed5eff143e639d2058871aa9ddc629b19a57
Ian Torres <itorres@umass.edu>
2017-11-05 05:51:33 -0500

    added: transitions.py

--- py/transitions.py
+++ py/transitions.py
@@ -37,4 +37,36 @@ markov["WP"] = []
 markov["WP$"] = []
 markov["WRB"] = []
 
-f = open('')
+f = open('../txt/bigram_counts.txt', 'r')
+
+isfound = False
+lines = []
+for l in f:
+    line = l
+    split = line.split(" ")
+    pos_trans, freq = split[0], split[1]
+    split = pos_trans.split(",")
+    first_pos, second_pos = split[0], split[1]
+    for markov_element in markov[first_pos]:
+        if markov_element[0] == second_pos:
+            markov_element = (second_pos, markov_element[1] + freq)
+            isfound = True
+            break
+
+    if not isfound:
+        markov[first_pos].append((first_pos, freq))
+        isfound = False
+
+    for markov_element in markov[second_pos]:
+        if markov_element[0] == first_pos:
+            markov_element = (second_pos, markov_element[1] + freq)
+            isfound = True
+            break
+
+    if not isfound:
+        markov[second_pos].append((second_pos, freq))
+        isfound = False
+
+print(markov)
+
+
