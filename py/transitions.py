7db24db76a9c092ff979993bb91ec8898c70a456
Ian Torres <itorres@umass.edu>
2017-11-05 09:16:04 -0500

    modified: transitions.py

--- py/transitions.py
+++ py/transitions.py
@@ -1,11 +1,44 @@
 
 import json
+from random import randint
+import operator
+
+sorted_pos_dict = dict()
+
+
+from random import random
+
+
+weights_list = []
+
+
+def select(container, weights):
+    total_weight = float(sum(weights))
+    rel_weight = [w / total_weight for w in weights]
+
+    # Probability for each element
+    probs = [sum(rel_weight[:i + 1]) for i in range(len(rel_weight))]
+
+    for (i, element) in enumerate(container):
+        if random() <= probs[i]:
+            break
+    return element
 
 
 def transition(pos):
-    with open('../txt/pos-transitions.txt') as data_file:
+    with open('../json/pos-transitions.json') as data_file:
         data = json.load(data_file)
-        return data[pos]
+        max_seq = 20
+        pos_seq = [pos]
 
+        while max_seq:
+            weights = data[a].values()
+            containers = data[a].keys()
+            next_pos = select(containers,weights)
+            if(next_pos=='.'):
+                break
+            pos_seq.append(next_pos)
+        pos_seq.append('.')
 
-# print(transition("CC"))
+        return pos_seq
+
